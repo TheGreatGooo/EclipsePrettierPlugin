66f74c5bfa9e312d5d8c3a67b1aabb6693c526e4
TheGreatGooo <thegreatgooo@gmail.com>
2020-08-18 19:59:50 -0400

    add node path and npm path

--- com.thegreatgooo.eclipse.prettier/src/com/thegreatgooo/eclipse/prettier/preferences/PreferenceConstants.java
+++ com.thegreatgooo.eclipse.prettier/src/com/thegreatgooo/eclipse/prettier/preferences/PreferenceConstants.java
@@ -7,6 +7,8 @@ public class PreferenceConstants {
 
 	public static final String P_NODE_PATH = "nodePathPreference";
 
+	public static final String P_NPM_PATH = "npmPathPreference";
+
 	public static final String P_TAB_WIDTH = "tabWidthgPreference";
 
 }
--- com.thegreatgooo.eclipse.prettier/src/com/thegreatgooo/eclipse/prettier/preferences/PreferenceInitializer.java
+++ com.thegreatgooo.eclipse.prettier/src/com/thegreatgooo/eclipse/prettier/preferences/PreferenceInitializer.java
@@ -1,5 +1,8 @@
 package com.thegreatgooo.eclipse.prettier.preferences;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
 
@@ -10,6 +13,9 @@ import com.thegreatgooo.eclipse.prettier.Activator;
  */
 public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
+	private static final String C_PROGRAM_FILES_NODEJS_NPM_CMD = "C:\\Program Files\\nodejs\\npm.cmd";
+	private static final String C_PROGRAM_FILES_NODEJS_NODE_EXE = "C:\\Program Files\\nodejs\\node.exe";
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -18,8 +24,39 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 	 */
 	public void initializeDefaultPreferences() {
 		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
-		store.setDefault(PreferenceConstants.P_NODE_PATH, "C:\\Program Files\\nodejs");
+		if (!isWindowsOs()) {
+			try {
+				Process whichNode = Runtime.getRuntime().exec("which node");
+				whichNode.waitFor(100, TimeUnit.MILLISECONDS);
+				String nodePath = new String(whichNode.getInputStream().readAllBytes());
+				if (nodePath.isBlank()) {
+					store.setDefault(PreferenceConstants.P_NODE_PATH, C_PROGRAM_FILES_NODEJS_NODE_EXE);
+				} else {
+					store.setDefault(PreferenceConstants.P_NODE_PATH, nodePath);
+				}
+			} catch (IOException | InterruptedException e) {
+				store.setDefault(PreferenceConstants.P_NODE_PATH, C_PROGRAM_FILES_NODEJS_NODE_EXE);
+			}
+			try {
+				Process whichNpm = Runtime.getRuntime().exec("which npm");
+				whichNpm.waitFor(100, TimeUnit.MILLISECONDS);
+				String npmPath = new String(whichNpm.getInputStream().readAllBytes());
+				if (npmPath.isBlank()) {
+					store.setDefault(PreferenceConstants.P_NPM_PATH, C_PROGRAM_FILES_NODEJS_NPM_CMD);
+				} else {
+					store.setDefault(PreferenceConstants.P_NPM_PATH, npmPath);
+				}
+			} catch (IOException | InterruptedException e) {
+				store.setDefault(PreferenceConstants.P_NPM_PATH, C_PROGRAM_FILES_NODEJS_NPM_CMD);
+			}
+		} else {
+			store.setDefault(PreferenceConstants.P_NODE_PATH, C_PROGRAM_FILES_NODEJS_NODE_EXE);
+			store.setDefault(PreferenceConstants.P_NPM_PATH, C_PROGRAM_FILES_NODEJS_NPM_CMD);
+		}
 		store.setDefault(PreferenceConstants.P_TAB_WIDTH, "2");
 	}
 
+	private boolean isWindowsOs() {
+		return System.getProperty("os.name").toLowerCase().contains("win");
+	}
 }
